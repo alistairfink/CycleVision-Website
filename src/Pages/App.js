3c74b7de3acd67f2898f6ed0df512c0d316f9bb2
Alistair Fink <alistairfink@gmail.com>
2019-11-02 12:55:25 -0400

    Team Members

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -1,5 +1,10 @@
 import React from "react";
 import meh from "../Resources/meh.png";
+import Sergio from "../Resources/Sergio.png";
+import Alistair from "../Resources/Alistair.png";
+import Elijah from "../Resources/Elijah.png";
+import Skye from "../Resources/Skye.png";
+import Zaki from "../Resources/Zaki.png";
 import "../Styling/App.css";
 
 function App() {
@@ -21,12 +26,39 @@ function App() {
           <h1>iBlind</h1>
           <h2>A Better Way to Bike</h2>
         </div>
-        <img className="Top-Inner-Img" src={meh}/>
+        <img className="Top-Inner-Img" src={meh} />
       </div>
       <div className="Section">
-        
+        <h2>About iBline</h2>
       </div>
-      <div className="test">test</div>
+      <div className="Section">
+        <h2>Team</h2>
+        <div className="Team">
+          <div className="Team-Member">
+            <img src={Alistair} />
+            <h3>Alistair Fink</h3>
+          </div>
+          <div className="Team-Member">
+            <img src={Elijah} />
+            <h3>Elijah Erb</h3>
+          </div>
+          <div className="Team-Member">
+            <img src={Sergio} />
+            <h3>Sergio Rodriguez</h3>
+          </div>
+          <div className="Team-Member">
+            <img src={Skye} />
+            <h3>Skye HoSue</h3>
+          </div>
+          <div className="Team-Member">
+            <img src={Zaki} />
+            <h3>Zaki Ahmed</h3>
+          </div>
+        </div>
+      </div>
+      <div className="Section">
+        <h2>Contact</h2>
+      </div>
     </div>
   );
 }
