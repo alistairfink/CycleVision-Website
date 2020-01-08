3c3613243b72fd71da82456da229893739fdc753
Alistair Fink <alistairfink@gmail.com>
2020-01-08 16:46:46 -0500

    organization in home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -10,6 +10,88 @@ import Loading from "../Resources/loading.svg";
 import "../Styling/Home.css";
 
 function Home() {
+
+  return (
+    <div className="App">
+      <div className="Header">
+        <div className="Header-Left">
+          <Link smooth to="#home" className="HashLink">
+            iBlind
+          </Link>
+        </div>
+        <div className="Header-Right">
+          <Link smooth to="#about" className="HashLink">
+            About
+          </Link>
+          <Link smooth to="#team" className="HashLink">
+            Team
+          </Link>
+          <Link smooth to="#contact" className="HashLink">
+            Contact
+          </Link>
+          <Link to="/portfolio" className="HashLink">
+            News
+          </Link>
+        </div>
+      </div>
+      <div className="Top" id="home">
+        <div className="Top-Inner">
+          <h1>iBlind</h1>
+          <h2>A Better Way to Bike</h2>
+        </div>
+        <img className="Top-Inner-Img" alt="Top Product" src={meh} />
+      </div>
+      <About />
+      <Team />
+      <Contact />
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div className="Section" id="about">
+      <h2>About iBlind</h2>
+    </div>
+  );
+}
+
+function Team() {
+  return (
+    <div className="Section" id="team">
+      <h2>Team</h2>
+      <div className="Team">
+        <div className="Team-Member">
+          <img src={Alistair} alt="Alistair Fink Profile" />
+          <h3>Alistair Fink</h3>
+          <h4>Software Engineer</h4>
+        </div>
+        <div className="Team-Member">
+          <img src={Elijah} alt="Elijah Erb Profile" />
+          <h3>Elijah Erb</h3>
+          <h4>Mechanical Engineer</h4>
+        </div>
+        <div className="Team-Member">
+          <img src={Sergio} alt="Sergio Rodriguez Profile" />
+          <h3>Sergio Rodriguez</h3>
+          <h4>Electrical Engineer</h4>
+        </div>
+        <div className="Team-Member">
+          <img src={Skye} alt="Skye HoSue Profile" />
+          <h3>Skye HoSue</h3>
+          <h4>Mascot</h4>
+        </div>
+        <div className="Team-Member">
+          <img src={Zaki} alt="Zaki Ahmed Profile" />
+          <h3>Zaki Ahmed</h3>
+          <h4>Software Engineer</h4>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Contact() {
   const [emailerName, setEmailerName] = useState("");
   const [emailerEmail, setEmailerEmail] = useState("");
   const [emailerSubject, setEmailerSubject] = useState("");
@@ -59,116 +141,53 @@ function Home() {
   };
 
   return (
-    <div className="App">
-      <div className="Header">
-        <div className="Header-Left">
-          <Link smooth to="#home" className="HashLink">
-            iBlind
-          </Link>
-        </div>
-        <div className="Header-Right">
-          <Link smooth to="#about" className="HashLink">
-            About
-          </Link>
-          <Link smooth to="#team" className="HashLink">
-            Team
-          </Link>
-          <Link smooth to="#contact" className="HashLink">
-            Contact
-          </Link>
-          <Link to="/portfolio" className="HashLink">
-            News
-          </Link>
-        </div>
+    <div className="Section Contact" id="contact">
+      <h2>Contact</h2>
+      <p>
+        Contact us using the contact form below or by emailing us at
+        sergiorodriguez2003@gmail.com.
+      </p>
+      <div className="Contact-Field">
+        <input
+          type="text"
+          value={emailerName}
+          placeholder="Name"
+          onChange={e => setEmailerName(e.target.value)}
+        />
       </div>
-      <div className="Top" id="home">
-        <div className="Top-Inner">
-          <h1>iBlind</h1>
-          <h2>A Better Way to Bike</h2>
-        </div>
-        <img className="Top-Inner-Img" alt="Top Product" src={meh} />
+      <div className="Contact-Field">
+        <input
+          type="text"
+          value={emailerEmail}
+          placeholder="Email"
+          onChange={e => setEmailerEmail(e.target.value)}
+        />
       </div>
-      <div className="Section" id="about">
-        <h2>About iBlind</h2>
+      <div className="Contact-Field">
+        <input
+          type="text"
+          value={emailerSubject}
+          placeholder="Subject"
+          onChange={e => setEmailerSubject(e.target.value)}
+        />
       </div>
-      <div className="Section" id="team">
-        <h2>Team</h2>
-        <div className="Team">
-          <div className="Team-Member">
-            <img src={Alistair} alt="Alistair Fink Profile" />
-            <h3>Alistair Fink</h3>
-            <h4>Software Engineer</h4>
-          </div>
-          <div className="Team-Member">
-            <img src={Elijah} alt="Elijah Erb Profile" />
-            <h3>Elijah Erb</h3>
-            <h4>Mechanical Engineer</h4>
-          </div>
-          <div className="Team-Member">
-            <img src={Sergio} alt="Sergio Rodriguez Profile" />
-            <h3>Sergio Rodriguez</h3>
-            <h4>Electrical Engineer</h4>
-          </div>
-          <div className="Team-Member">
-            <img src={Skye} alt="Skye HoSue Profile" />
-            <h3>Skye HoSue</h3>
-            <h4>Mascot</h4>
-          </div>
-          <div className="Team-Member">
-            <img src={Zaki} alt="Zaki Ahmed Profile" />
-            <h3>Zaki Ahmed</h3>
-            <h4>Software Engineer</h4>
-          </div>
-        </div>
+      <div className="Contact-Field">
+        <textarea
+          type="text"
+          value={emailerMessage}
+          placeholder="Message"
+          onChange={e => setEmailerMessage(e.target.value)}
+        />
       </div>
-      <div className="Section Contact" id="contact">
-        <h2>Contact</h2>
-        <p>
-          Contact us using the contact form below or by emailing us at
-          sergiorodriguez2003@gmail.com.
-        </p>
-        <div className="Contact-Field">
-          <input
-            type="text"
-            value={emailerName}
-            placeholder="Name"
-            onChange={e => setEmailerName(e.target.value)}
-          />
-        </div>
-        <div className="Contact-Field">
-          <input
-            type="text"
-            value={emailerEmail}
-            placeholder="Email"
-            onChange={e => setEmailerEmail(e.target.value)}
-          />
-        </div>
-        <div className="Contact-Field">
-          <input
-            type="text"
-            value={emailerSubject}
-            placeholder="Subject"
-            onChange={e => setEmailerSubject(e.target.value)}
-          />
-        </div>
-        <div className="Contact-Field">
-          <textarea
-            type="text"
-            value={emailerMessage}
-            placeholder="Message"
-            onChange={e => setEmailerMessage(e.target.value)}
-          />
-        </div>
-        {emailerLoading ? (
-          <img
-            className="Contact-Loading"
-            src={Loading}
-            alt="Send Email Loading"
-          />
-        ) : (
-          <button onClick={() => sendEmail()}>Send</button>
-        )}
-      </div>
+      {emailerLoading ? (
+        <img
+          className="Contact-Loading"
+          src={Loading}
+          alt="Send Email Loading"
+        />
+      ) : (
+        <button onClick={() => sendEmail()}>Send</button>
+      )}
       <div
         className={
           emailerSuccess ? "Contact-Email-Shown" : "Contact-Email-Hidden"
